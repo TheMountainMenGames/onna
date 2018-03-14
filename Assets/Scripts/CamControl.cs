ecc5ad61f00866460ab4418d7e0496121c8e151b
jessevandenberg <jessevandenberg@gmail.com>
2018-03-14 22:18:24 +0100

    update to scroll
    Scroll correct when camera is turned, except for back and forward.

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script controls the camera rotation, zoom and scroll
+
 public class CamControl : MonoBehaviour {
 
+    //declare variables
     public float scrollSpeed = 100;
     public float zoomSpeed = 1000;
     public float rotateSpeed = 200;
-    public float downPan = 10;
     public float forwardPan = 2;
     public float cameraAngle = 60;
-    
-    public float minY = -90.0f;
-    public float maxY = 90.0f;
-    
-    float rotationY = 0.0f;
-    float rotationX = 0.0f;
+    public float maxzoomout = 1;
+    public float maxzoomin = 1;
 
+    private float rotationX = 0;
+    
     // Use this for initialization
     void Start () {
-		
-	}
+        
+    }
 
     // Update is called once per frame
     void LateUpdate() {
         /* todo list
          * 1. add scroll boundaris to level size
          * 2. add zoom levels with pan
+         * 3. add max and min zoom
          */
+        
+        //Debug.Log(Vector3.up);
 
         //declare variables
         float horizontal = Input.GetAxis("Horizontal");
@@ -36,8 +39,13 @@ public class CamControl : MonoBehaviour {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
+        //define cameradirection
+        Vector3 cameraDirection = new Vector3(horizontal, 0f, vertical);
+        cameraDirection = Camera.main.transform.TransformDirection(cameraDirection);
+        cameraDirection.y = 0.0f;
+                
         //scroll using arrows or wasd
-        transform.position += (Vector3.forward * vertical + Vector3.right * horizontal) * Time.deltaTime * scrollSpeed;
+        transform.position += cameraDirection * Time.deltaTime * scrollSpeed;
 
         //zoom using mousewheel scroll
         if (!Input.GetMouseButton(2)) //check if mousewheel is not clicked
@@ -45,30 +53,30 @@ public class CamControl : MonoBehaviour {
             if (mouseWheel != 0)
             {
                 //zoom up or down
-                transform.position += ((Vector3.down * downPan) + (Vector3.forward * forwardPan)) * mouseWheel * Time.deltaTime * zoomSpeed;
+                transform.position += ((Vector3.down) + (Vector3.forward * forwardPan)) * mouseWheel * Time.deltaTime * zoomSpeed;
             }
 
             //scroll on edge of screen
             if (Input.mousePosition.x >= Screen.width - 20)
             {
                 //scroll right
-                transform.position += Vector3.right * Time.deltaTime * scrollSpeed;
+                transform.Translate(Vector3.right * Time.deltaTime * scrollSpeed);
             }
             else if (Input.mousePosition.x <= 20 && transform.position.x > 20)
             {
                 //scroll left
-                transform.position += Vector3.left * Time.deltaTime * scrollSpeed;
+                transform.Translate(Vector3.left * Time.deltaTime * scrollSpeed);
             }
 
             if (Input.mousePosition.y >= Screen.height - 20 && transform.position.y > 20)
             {
                 //scroll forward
-                transform.position += Vector3.forward * Time.deltaTime * scrollSpeed;
+                transform.Translate(Vector3.forward * Time.deltaTime * scrollSpeed);
             }
             else if (Input.mousePosition.y <= 20)
             {
                 //scroll back
-                transform.position += Vector3.back * Time.deltaTime * scrollSpeed;
+                transform.Translate(Vector3.back * Time.deltaTime * scrollSpeed);
             }
         }
         
@@ -76,7 +84,7 @@ public class CamControl : MonoBehaviour {
         if (Input.GetMouseButton(2))
         {
             rotationX += mouseX * Time.deltaTime * rotateSpeed;
-            transform.localEulerAngles = new Vector3(cameraAngle, rotationX, 0);
-        }
+            transform.localEulerAngles = new Vector3(transform.eulerAngles.x, rotationX, 0);
+        }      
     }
 }
